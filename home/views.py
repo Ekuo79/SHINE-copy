71c50bd9a87f13485d32df27960d3caccae56229
Eric Kuo <112114007+Ekuo79@users.noreply.github.com>
2025-06-29 16:44:40 -0400

    Modern Website Code Revamp

--- home/views.py
+++ home/views.py
@@ -4,10 +4,15 @@ from datetime import datetime
 from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from companies.models import Companies
+
 class HomeView(TemplateView):
     template_name = 'home/welecome.html'
-    extra_context = {'today': datetime.today()}
-    extra_context['companies'] = Companies.objects.all()
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['today'] = datetime.today()
+        context['companies'] = Companies.objects.all()
+        return context
 
 class AuthorizedView(LoginRequiredMixin, TemplateView):
     template_name = 'home/authorized.html'
